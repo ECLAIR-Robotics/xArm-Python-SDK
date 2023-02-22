16269ff609851e22d5f97c38ef9c0174bd43589f
Emerald-Z <ejzhang2004@gmail.com>
2023-02-21 21:22:35 -0600

    demo commit pushed loader and model

--- pcr/gizzmos.py
+++ pcr/gizzmos.py
@@ -1,4 +1,10 @@
 from xarm.wrapper import XArmAPI
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from PIL import Image
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
 
 '''
     Gizzmos are external hardware elements that assist the robotic arm
@@ -47,3 +53,67 @@ class AtalantaModule:
 
     def check_connection(self) -> bool:
         return True
+    
+'''
+    Data model and loader for OCR on the pipette dial 
+    - model located in 'mnist1-ce-sgd-3.pt'
+'''
+
+input_size = 784 # 28 * 28 input image size
+hidden_sizes = [128, 64]
+output_size = 10
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_size, hidden_sizes[0]),
+            nn.ReLU(),
+            nn.Linear(hidden_sizes[0], hidden_sizes[1]),
+            nn.ReLU()
+        )
+        self.last_layer = nn.Linear(hidden_sizes[1], output_size)
+
+    def forward(self, x):
+        logits = self.model(x)
+        logits = self.last_layer(logits)
+        return logits
+
+    def predict(self, x):
+        logits = self.model(x)
+        logits = self.last_layer(logits)
+        softmax = torch.nn.Softmax(dim=1)
+        return softmax(logits)
+
+# get input image
+# replace image path with your image
+image_path = 'printed_digit/train/3/0_0_7.jpeg'
+# processing -> resize and turn B&W (white on black)
+
+og_img = Image.open(image_path)
+img = transforms.Compose([transforms.ToTensor(),
+                            transforms.Resize((28, 28)),
+                            transforms.Grayscale(1),
+                            transforms.Normalize((0.5,), (0.5,))])(og_img)
+
+input = img.reshape(-1, 784) 
+
+# load model
+model_dir = 'models/transfer-ce-sgd-4.pt'
+state = torch.load(model_dir)
+model = Net()
+try:
+    state.eval()
+except AttributeError as error:
+    print(error)
+
+model.load_state_dict(state['model_state_dict'])
+model.eval()
+
+# make prediction
+with torch.no_grad():
+    output = model.predict(input)
+
+print(output)
+
+print(torch.argmax(output))
